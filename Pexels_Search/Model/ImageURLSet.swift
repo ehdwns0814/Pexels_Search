9703aa5ef45d44b6c44fd1f5f2a468094ae93878
DongJun96 <djlee1201@naver.com>
2024-08-03 17:31:13 +0900

    feat: ImageURLSet 구현

---
+++ Pexels_Search/Model/ImageURLSet.swift
@@ -0,0 +1,19 @@
+//
+//  ImageURLSet.swift
+//  Pexels_Search
+//
+//  Created by 동준 on 8/3/24.
+//
+
+import Foundation
+
+struct ImageURLSet {
+    let original: String
+    let large2x: String
+    let large: String
+    let medium: String
+    let small: String
+    let portrait: String
+    let landscape: String
+    let tiny: String
+}
