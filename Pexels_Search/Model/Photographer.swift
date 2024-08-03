c532851d5830e485ca4a250bf0bed85cf4b53598
DongJun96 <djlee1201@naver.com>
2024-08-03 17:30:52 +0900

    feat: Photographer 구현

---
+++ Pexels_Search/Model/Photographer.swift
@@ -0,0 +1,14 @@
+//
+//  Photographer.swift
+//  Pexels_Search
+//
+//  Created by 동준 on 8/3/24.
+//
+
+import Foundation
+
+struct Photographer {
+    let name: String
+    let profileURL: String
+    let id: Int
+}
